0e6c4aadef9b9c99bcc1e12a428ae6e1c1564d97
Muhammad Yusuf <yusuf@iamyuu.dev>
2023-08-21 10:37:44 +0700

    ⚡️ prerender errors page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output: "server",
+  output: "hybrid",
   adapter: cloudflare()
 });
